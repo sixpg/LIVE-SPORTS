130aca4a600aef1a69b72067472457210f5c81de
sixpg <76198021+sixpg@users.noreply.github.com>
2026-06-29 23:51:51 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,5 +6,5 @@ SOURCE = os.environ["SOURCE_URL"]
 with urllib.request.urlopen(SOURCE) as response:
     playlist = response.read().decode("utf-8")
 
-with open("playlist.m3u8", "w", encoding="utf-8") as f:
+with open("fifa2k26.m3u8", "w", encoding="utf-8") as f:
     f.write(playlist)
